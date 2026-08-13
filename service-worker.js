d5267d8b4c6eb7758151cd4976c0ed5aad874626
manpazito <manpazito@berkeley.edu>
2026-08-13 10:55:33 -0700

    Use dumbbell icon for strength navigation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "reproot-shell-v4";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "reproot-shell-v5";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./dumbell-fitness-svgrepo-com.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
